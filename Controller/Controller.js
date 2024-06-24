badcf73f3a6542c0a5dab22f35d46985e94ad482
Vinit Pandit <vinitsharma2480@gmail.com>
2024-06-24 08:37:41 +0530

    fixing Controller

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,4 +1,4 @@
-const { userDTO } = require("../DAO/userDAO");
+const { userDTO } = require("../DTO/userDto");
 const { responseData } = require("../DTO/responseDto")
 const { userServices } = require("../Services/userServices")
 
@@ -6,78 +6,84 @@ const userController = {
     listUser: async function (req, res) {
         let message = "All done"
         try {
-            const result = userServices.listUser();
+            const result = await userServices.listUser();
             const resDto = responseData(message, result)
             res.status(201).json(resDto)
 
         }
         catch (err) {
-            message = err;
+            console.log(err)
+            message = err.message;
             const resDto = responseData(message, {})
             res.status(501).json(resDto)
         }
-        return result;
     },
     creatUser: async function (req, res) {
         let message = "All done";
         let data = req.body.reqData;
         try {
             const reqDto = userDTO(data)
-            const result = userServices.creatUser(reqDto);
+            const result = await userServices.creatUser(reqDto);
             const resDto = responseData(message, result)
             res.status(201).json(resDto)
 
         }
         catch (err) {
-            message = err;
+            console.log(err)
+            message = err.message;
             const resDto = responseData(message, {})
             res.status(501).json(resDto)
         }
-        return result;
+       
     },
     updateUser: async function (req , res) {
         // validating the data 
         let message = "All done";
         let data = req.body.reqData;
         try {
-            const result = userServices.updateUser(data)
+            const result = await userServices.updateUser(data)
             const resDto = responseData(message, result)
             res.status(201).json(resDto)
         }
         catch (err) {
-            message = err
+            console.log(err)
+            message = err.message
             const resDto = responseData(message, {})
+            console.log(message)
+            console.log(resDto)
             res.status(501).json(resDto)
         }
-        return result
+        
     },
     getUser: async function (req , res) {
         let message = "All done";
         let data = req.body.reqData;
         try {
-            const result = userServices.getUser(data)
+            const result = await userServices.getUser(data)
             const resDto = responseData(message, result)
             res.status(201).json(resDto)
-            return result
+            
         }
         catch (err) {
-            message = err
+            console.log(err)
+            message = err.message
             const resDto = responseData(message, {})
             res.status(501).json(resDto)
         }
         
     },
-    deleteUser: async function (Data) {
+    deleteUser: async function (req , res) {
         let message = "All done";
         let data = req.body.reqData;
         try {
-            const result = userServices.updateUser(data)
+            const result = await userServices.deleteUser(data)
             const resDto = responseData(message, result)
             res.status(201).json(resDto)
-            return result
+
         }
         catch (err) {
-            message = err
+            console.log(err)
+            message = err.message
             const resDto = responseData(message, {})
             res.status(501).json(resDto)
         }
